a798e3d4531db5254ecf93a665511778396cd861
Geo Carncross <geocar@gmail.com>
2016-05-21 19:23:40 +0100

    use document instead

--- src/imds.erl
+++ src/imds.erl
@@ -2,14 +2,12 @@
 -export([region/0, iam/1]).
 
 text(Path) ->
-  {ok,200,_,ClientRef}=hackney:get(<<"http://169.254.169.254/latest/meta-data/", Path/binary>>, []),
+  {ok,200,_,ClientRef}=hackney:get(<<"http://169.254.169.254/latest/", Path/binary>>, []),
   {ok, Body} = hackney:body(ClientRef),
   Body.
 json(Path) -> jsone:decode(text(Path), [{object_format,proplist}]).
 
-region() -> text(<<"placement/availability-zone">>).
-iam(Name) -> json(<<"iam/security-credentials/", Name/binary>>) ++ [{<<"Name">>, Name}].
-  
+region() -> proplists:get_value(<<"region">>, json(<<"dynamic/instance-identity/document">)).
 
-
+iam(Name) -> json(<<"meta-data/iam/security-credentials/", Name/binary>>) ++ [{<<"Name">>, Name}].
 
