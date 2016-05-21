3af2a327cfe98b19ee38a27dcaf17c44383aff9e
Geo Carncross <geocar@gmail.com>
2016-05-21 18:51:02 +0100

    proplist

--- src/imds.erl
+++ src/imds.erl
@@ -5,7 +5,7 @@ text(Path) ->
   {ok,200,_,ClientRef}=hackney:get(<<"http://169.254.169.254/latest/meta-data/", Path/binary>>, []),
   {ok, Body} = hackney:body(ClientRef),
   Body.
-json(Path) -> jsone:decode(text(Path)).
+json(Path) -> jsone:decode(text(Path), [{object_format,proplist}]).
 
 region() -> text(<<"placement/availability-zone">>).
 iam(Name) -> json(<<"iam/security-credentials/", Name/binary>>).
