aff5d41be773b8e4b4cce897f08a1d7b02e6ea7d
Geo Carncross <geocar@gmail.com>
2016-09-08 07:38:36 -0400

    ac

--- src/imds.erl
+++ src/imds.erl
@@ -1,7 +1,8 @@
 -module(imds).
 -export([region/0, iam/2]).
 
--define(OVERLAPIAM, 3600).
+% http://docs.aws.amazon.com/AWSEC2/latest/WindowsGuide/iam-roles-for-amazon-ec2.html#instance-metadata-security-credentials
+-define(OVERLAPIAM, 300).
 
 text(Path) ->
   {ok,200,_,ClientRef}=hackney:get(<<"http://169.254.169.254/latest/", Path/binary>>, []),
@@ -13,12 +14,12 @@ region() -> proplists:get_value(<<"region">>, json(<<"dynamic/instance-identity/
 
 iam(P, Name) ->
   Info = json(<<"meta-data/iam/security-credentials/", Name/binary>>) ++ [{<<"Name">>, Name}],
+  Now = calendar:datetime_to_gregorian_seconds(erlang:universaltime()),
   Expiration = proplists:get_value(<<"Expiration">>, Info),
-  Expires = case is_binary(Expiration) of true -> calendar:datetime_to_gregorian_seconds(iso8601:parse(Expiration)); _ -> undefined end,
+  Expires = case is_binary(Expiration) of true -> calendar:datetime_to_gregorian_seconds(iso8601:parse(Expiration)); _ -> Now + 600 end,
   P!{self(), {proplists:get_value(<<"AccessKeyId">>, Info),
     proplists:get_value(<<"SecretAccessKey">>, Info),
     proplists:get_value(<<"Token">>, Info),
     proplists:get_value(<<"Name">>, Info)}},
-  Now = calendar:datetime_to_gregorian_seconds(erlang:universaltime()),
-  timer:sleep((Expires - Now - round(?OVERLAPIAM * 0.9)) * 1000),
+  timer:sleep((Expires - Now - ?OVERLAPIAM + 1) * 1000),
   iam(P, Name).
